5dbb828a6e4f8660e98b04843cd7fce354fd545f
LokeshChowdary05 <87775654+LokeshChowdary05@users.noreply.github.com>
2025-03-20 20:11:01 -0500

    Update CalculatorTest.java

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -35,6 +35,7 @@ public class Calculator {
                 break;
             default:
                 System.out.println("Invalid operator.");
+                System.out.println("Success");
                 return;
         }
 
